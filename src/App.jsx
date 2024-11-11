23142ec79e1ebab7db762f5c27cbc47628c8c764
Astrid Ge <qiutongg@umich.edu>
2024-11-10 20:53:36 -0800

    complete everything

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Bookshelf from './components/Bookshelf';
 
 
 const App = () => {
+  
   return (
     <>
       <h1>My Bookshelf</h1>
--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -1,54 +1,73 @@
 import '../index.css';
 import { useState } from 'react';
 
-const Bookshelf = () => {
+const Bookshelf = (props) => {
     const [books, setBooks] = useState([
         { title: 'Fourth Wing', author: 'Rebecca Yarros' },
         { title: 'The Lion, the Witch and the Wardrobe', author: 'C.S. Lewis' },
       ]);
 
-    const [newbook, setNewBook] = useState([])
+    const [newBook, setNewBook] = useState({
+        title: "",
+        author: ""
+    })
+
+    const handleInput = (event) => {
+        setNewBook({...newBook, [event.target.name]: event.target.value})
+
+    }
+
+    
+    const handelSubmit = (event) => {
+        event.preventDefault()
+        console.log(event)
+        setBooks([...books, newBook])
+        setNewBook({
+            title: "",
+            author: ""
+        })
+        
+    }
 
     return(
-        <div className="bookshelfDiv">
-            <div className="formDiv">
+        <div className="bookshelfDiv" >
+            <div className="formDiv" >
                 <h3>Add a Book</h3>
-                <form>
-                    <div>
-                        <label>Title:</label>
-                        <input
-                            id="title"
-                            name="title"
-                        ></input>
-                    </div>
-                    <div>
-                        <label>Author:</label>
-                        <input
-                            id="author"
-                            name="author"
-                        ></input>
-                    </div>
-                </form>
-                <button>Add book</button>
+                {/* form will automatically have an id and the button needs to have the same id in order to control the form */}
+                <form onSubmit={handelSubmit}>
+                    <label htmlFor="title">Title:</label>
+                    <input
+                        id="title"
+                        name="title"
+                        value={newBook.title}
+                        onChange={handleInput}
+                    ></input>
+    
+                    <label htmlFor="author">Author:</label>
+                    <input
+                        id="author"
+                        name="author" 
+                        value={newBook.author}
+                        onChange={handleInput}
+                    ></input>
+    
+                    <button type="submit">Add book</button>
 
+                </form>
             </div>
-            
+    
             <div className="bookCardsDiv">
-                {books.map((eachBook)=> {
+                {books?.map((eachBook, index)=> {
                     return(
-                        <div className="bookCard">
+                        <div className="bookCard" key={index}>
                             <h3>{eachBook.title}</h3>
                             <p>by {eachBook.author}</p>
                         </div>
                     )
                 })}      
-
             </div>
-
         </div>
-
     )
-
 }
 
 
